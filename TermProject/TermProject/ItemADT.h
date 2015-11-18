7e96dafbe5f82539cb32ed83b356b38d35803400
daviddorsey <Hcky739*>
2015-11-17 23:04:47 -0500

    initial commit

---
+++ TermProject/TermProject/ItemADT.h
@@ -0,0 +1,13 @@
+//
+//  ItemADT.h
+//  TermProject
+//
+//  Created by David Dorsey on 11/17/15.
+//  Copyright (c) 2015 David Dorsey. All rights reserved.
+//
+
+#ifndef TermProject_ItemADT_h
+#define TermProject_ItemADT_h
+
+
+#endif
---
+++ TermProject/TermProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TermProject
+//
+//  Created by David Dorsey on 11/17/15.
+//  Copyright (c) 2015 David Dorsey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    
+    return 0;
+}
