3f879f2f306cf5586859a2ed17ae8c0e699a3590
daviddorsey <Hcky739*>
2015-11-18 11:25:58 -0500

    wrote ItemADT.h

--- TermProject/TermProject/ItemADT.h
+++ TermProject/TermProject/ItemADT.h
@@ -9,5 +9,25 @@
 #ifndef TermProject_ItemADT_h
 #define TermProject_ItemADT_h
 
+class ItemADT{
+    
+public:
+    
+    //Restocks an item by buying more, reducing revenue for the day
+    //int numCopies - number of items that the store is buying
+    //int buyPrice - price of each item
+    virtual void buy(int numCopies, int buyPrice) = 0;
+    
+    //Sells one copy of an item, reducing that item's stock by one and increasing revenue for the day
+    virtual void sell() = 0;
+    
+    //Removes an item from the shelves, setting numInStock to 0
+    virtual void remove() = 0;
+    
+    //Chagnes the price of an item
+    //int price - new price that the item will be set to
+    virtual void setPrice(int price) = 0;
+    
+};
 
 #endif
