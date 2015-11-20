9e7c2f3b5a874525647b1cee738f37459f9d278a
daviddorsey <Hcky739*>
2015-11-20 01:02:13 -0500

    wrote console.cpp, made minor changes to console.h and ItemADT.h

--- TermProject/TermProject/Console.h
+++ TermProject/TermProject/Console.h
@@ -32,11 +32,35 @@ private:
     bool preowned;
     
 public:
-    //default constructor
-    Console();
-    //overloaded constructor
-    Console(float priceIn, std::string titleIn, std::string editionIn, std::string manufacturerIn, int warrantyIn, bool preownedIn);
-    void toString();
+    //overloaded constructor with default values
+    Console(int stockIn=0, float priceIn=0, std::string titleIn="", std::string editionIn="default", std::string manufacturerIn="", int warrantyIn=0, bool preownedIn=false);
+    
+    //buys more copies of the console to put on the shelves
+    void buy(int numCopies);
+    
+    //sells console(s) to a customer in the store
+    void sell(int numCopies);
+    
+    //takes a console off the shelves so it's no longer available for purchase
+    void remove();
+    
+    //changes the price of the console
+    void setPrice(float priceIn);
+    
+    //formats an object's information in a readable string
+    std::string toString();
+    
+    //returns the console's title
+    std::string getTitle();
+    
+    //changes the console's title
+    void setTitle(std::string titleIn);
+    
+    //returns the console's edition
+    std::string getEdition();
+    
+    //returns the console's preowned status - true if it was preowned, false if it is new
+    bool getPreowned();
     
 };
 
