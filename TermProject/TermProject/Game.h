b3473e1a4c6626c77a0e4540be6ec9478c38af19
dannykustin <dannykustin@gmail.com>
2015-11-19 22:02:25 -0500

    started the game class  stuff

--- TermProject/TermProject/Game.h
+++ TermProject/TermProject/Game.h
@@ -10,5 +10,26 @@
 #define __TermProject__Game__
 
 #include <stdio.h>
+#include <iostream>
+#include "ItemADT.h"
+#include "QueueADT.h"
 
 #endif /* defined(__TermProject__Game__) */
+class Game : public ItemADT{
+private:
+    int numInStock;
+    float price;
+    std:: string title;
+    std:: string genre;
+    std:: string rating;
+    bool preOrder;
+    QueueADT* waitingList;
+    std:: string publisher;
+    bool preOwned;
+    
+public:
+    std:: string toString();
+    void comeToStock();
+    void preOrderGame(std:: string name);
+    void removePreOrder(std:: string name);
+};
